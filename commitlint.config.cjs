286e28a4bfad7c1f9699b3af8858e84fd3c10601
shuymn <mail@shuymn.me>
2025-09-06 18:05:04 +0900

    chore(commitlint): remove custom header length rule

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,4 @@
 /** @type {import("@commitlint/types").UserConfig} */
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    // Enforce project-wide 50-char header limit
-    "header-max-length": [2, "always", 50],
-  },
 };
